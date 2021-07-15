3249ac1256ef761ef5c33a3cdb624df7783261b1
Nshan Vardanyan <Nshan.Vardanyan.1996@gmail.com>
2021-07-15 11:34:17 +0400

    db structured

---
+++ database/migrations/2021_07_15_071853_create_schedules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("teacher_id")->index();
+            $table->unsignedBigInteger("class_id")->index();
+            $table->timestamp("day")->index();
+            $table->timestamp("start_time");
+            $table->timestamp("end_time");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
